728922d5b599e47d9d3007e1b6f9d662e0b9553e
Arpit <arpited7@gmail.com>
2021-11-05 22:45:15 +0545

    anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -7,7 +7,29 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+function anagrams(stringA, stringB) {
+    let obj1 = charMap(stringA)
+    let obj2 = charMap(stringB)
 
-function anagrams(stringA, stringB) {}
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false
 
-module.exports = anagrams;
+    for (let key in obj1) {
+        if (obj1[key] !== obj2[key]) return false
+        return true
+        // if the no of occurence of each character matches then we know that both strings consist 
+        // of the same characters
+    }
+}
+
+let charMap = str => {
+    let obj = {}
+    for (char of str.replace(/[^\w]/g, "").toLowerCase()) {
+        obj[char] ? obj[char] += 1 : obj[char] = 1
+    }
+    return obj
+}
+
+let cleanUp = str => str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('')
+let anagrams2 = (string1, string2) => cleanUp(string1) === cleanUp(string2)
+
+module.exports = anagrams
